31827e48c6e4e9879c708026e22ab36f6f0b0a74
Parvaneh <Parva9eh@hotmail.com>
2023-07-05 16:05:45 -0700

    Fixed the render issue

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-const rootElement = document.getElementById('root');
-
-render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  rootElement
 );
 
