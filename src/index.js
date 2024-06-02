02b3e9083625d14d25a8648960bfbdaf71cde92c
Parvaneh <Parva9eh@hotmail.com>
2024-06-01 21:21:54 -0700

    Added redux, react-redux, and user related redux code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import {UserProvider} from './context/user.context';
+import {Provider} from 'react-redux';
 import {CategoriesProvider} from './context/categories.context';
 import { CartProvider } from './context/cart.context';
+import {store} from './store/store';
 
 import './index.scss';
 import App from './App';
@@ -12,15 +13,15 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 );
 
