552492e4c82135f7f541c56a174b66045b25bb9b
Parvaneh <Parva9eh@hotmail.com>
2024-06-05 17:43:40 -0700

    Added redux-thunk for fetching categories

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,6 +1,23 @@
 import {createAction} from '../../utils/reducer/reducer.utils';
 import { CATEGORIES_ACTION_TYPES } from './category.types';
+import { getCollectionAndDocuments } from '../../utils/firebase/firebase.utils';
 
-export const setCategories = (categories) =>
-    createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES,categories);
-  
+export const fetchCategoriesStart = () =>
+createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+
+export const fetchCategoriesSuccess = (categoriesArray) =>
+createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categoriesArray);
+
+export const fetchCategoriesFailed = (error) =>
+createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, error);
+
+export const fetchCategoriesStartAsync = () => async (dispatch) => {
+    dispatch(fetchCategoriesStart());
+    try{
+        const categoriesArray = await getCollectionAndDocuments();
+        dispatch(fetchCategoriesSuccess(categoriesArray));
+    }
+    catch(error){
+        dispatch(fetchCategoriesFailed(error));
+    }
+} 
