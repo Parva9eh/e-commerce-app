5eb60eec41c94c2563e80f40bec719e73213b6e3
Parvaneh <Parva9eh@hotmail.com>
2024-06-06 16:17:14 -0700

    Updated the user saga to use signInWithGooglePopup

--- src/store/store.js
+++ src/store/store.js
@@ -1,22 +1,21 @@
 import {compose, createStore, applyMiddleware} from 'redux';
-import {logger} from 'redux-logger';
-import {rootReducer} from './root-reducer';
 import { persistStore, persistReducer } from 'redux-persist';
+import logger from 'redux-logger';
 import storage from 'redux-persist/lib/storage';
-//import {thunk} from 'redux-thunk';
 import createSagaMiddleWare from 'redux-saga';
 import { rootSaga } from './root-saga';
+import { rootReducer } from './root-reducer';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart']
+};
 
 const sagaMiddleWare = createSagaMiddleWare();
 const middleWares = [process.env.NODE_ENV !== 'production' && logger, sagaMiddleWare].filter(
     Boolean
   );
-
-const persistConfig = {
-  key: 'root',
-  storage,
-  blacklist: ['user']
-}
 
 const composeEnhancer =
   (process.env.NODE_ENV !== 'production' &&
