af650d690c390619a4cb15f42d14fa6565a07cba
Parvaneh <Parva9eh@hotmail.com>
2024-06-11 21:48:59 -0700

    Updated reducer.utils to use Machable type and withMacher function

--- src/utils/reducer/reducer.utils.ts
+++ src/utils/reducer/reducer.utils.ts
@@ -1,4 +1,24 @@
+import { object } from "prop-types";
 import { AnyAction } from "redux-saga";
+
+type Machable<AC extends ()=> AnyAction> = AC & {
+    type: ReturnType<AC>['type'];
+    match(action: AnyAction): action is ReturnType<AC>
+}
+
+export function withMatcher<AC extends () => AnyAction & {type: string}>(actionCreator: AC): Machable<AC>;
+
+export function withMatcher<AC extends (...args: any[]) => AnyAction & {type: string}>(actionCreator: AC): Machable<AC>;
+
+export function withMatcher(actionCreator: Function){
+    const type = actionCreator().type;
+    return Object.assign(actionCreator,{
+        type,
+        match(action: AnyAction) {
+            return action.type === type;
+        }
+    })
+}
 
 export type ActionWithPaylod<T, P> = {
     type: T;
@@ -11,7 +31,7 @@ export type Action<T> = {
 
 export function createAction<T extends string, P>(type: T,payload: P): ActionWithPaylod<T, P>;
 
-export function createAction<T extends string, P> (type: T,payload: P): Action<T>;
+export function createAction<T extends string, P> (type: T,payload: void): Action<T>;
 
 export function createAction<T extends string, P> (type: T,payload: P) {
     return {type,payload} 
