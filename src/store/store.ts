c0b3c8ae04524ca17c1cfb9a827c5de40ad7fd2d
Parvaneh <22968390+Parva9eh@users.noreply.github.com>
2024-06-12 15:21:01 -0700

    Converted the store to ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,24 @@
-import {compose, createStore, applyMiddleware} from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
+import { compose, createStore, applyMiddleware, Middleware } from 'redux';
+import { persistStore, persistReducer, PersistConfig } from 'redux-persist';
 import logger from 'redux-logger';
 import storage from 'redux-persist/lib/storage';
 import createSagaMiddleWare from 'redux-saga';
 import { rootSaga } from './root-saga';
 import { rootReducer } from './root-reducer';
 
-const persistConfig = {
+export type RootState = ReturnType<typeof rootReducer>;
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+type ExtendedPersistConfig = PersistConfig<RootState> & {
+  whitelist: (keyof RootState)[];
+};
+
+const persistConfig: ExtendedPersistConfig = {
   key: 'root',
   storage,
   whitelist: ['cart']
@@ -14,7 +26,7 @@ const persistConfig = {
 
 const sagaMiddleWare = createSagaMiddleWare();
 const middleWares = [process.env.NODE_ENV !== 'production' && logger, sagaMiddleWare].filter(
-    Boolean
+  (middleware): middleware is Middleware => Boolean(middleware)
   );
 
 const composeEnhancer =
