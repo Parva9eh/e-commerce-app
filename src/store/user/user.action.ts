47ef7983d6b01076751aa690a619f8a984b679c3
Parvaneh <Parva9eh@hotmail.com>
2024-06-12 19:41:15 -0700

    Adjusted user action

--- src/store/user/user.action.ts
+++ src/store/user/user.action.ts
@@ -18,13 +18,13 @@ export type SetCurrentUser = ActionWithPayload<USER_ACTION_TYPES.SET_CURRENT_USE
 
 export type GoogleSignInStart = Action<USER_ACTION_TYPES.GOOGLE_SIGN_IN_START>;
 
-export type EmailSignInStart = ActionWithPayload<USER_ACTION_TYPES.EMAIL_SIGN_IN_START, {email: string, password: string}>;
+export type EmailSignInStart = ActionWithPayload<USER_ACTION_TYPES.EMAIL_SIGN_IN_START, { email: string; password: string }>;
 
 export type SignInSuccess = ActionWithPayload<USER_ACTION_TYPES.SIGN_IN_SUCCESS, UserData>;
 
 export type SignInFailed = ActionWithPayload<USER_ACTION_TYPES.SIGN_IN_FAILED, Error>;
 
-export type SignUpStart = ActionWithPayload<USER_ACTION_TYPES.SIGN_UP_START, { email: string }>;
+export type SignUpStart = ActionWithPayload<USER_ACTION_TYPES.SIGN_UP_START, { email: string; password: string; displayName: string }>;
 
 export type SignUpSuccess = ActionWithPayload<USER_ACTION_TYPES.SIGN_UP_SUCCESS, { user: User; additionalDetails: AdditionalInformation }>;
 
@@ -48,14 +48,14 @@ export const googleSignInStart = withMatcher((): GoogleSignInStart =>
 export const emailSignInStart = withMatcher((email: string, password: string): EmailSignInStart =>
     createAction(USER_ACTION_TYPES.EMAIL_SIGN_IN_START,{ email, password }));
 
-export const signInSuccess = withMatcher((user: UserData): SignInSuccess =>
+export const signInSuccess = withMatcher((user: UserData & {id: string}): SignInSuccess =>
     createAction(USER_ACTION_TYPES.SIGN_IN_SUCCESS, user));
 
 export const signInFailed = withMatcher((error: Error): SignInFailed =>
     createAction(USER_ACTION_TYPES.SIGN_IN_FAILED, error));
 
 export const signUpStart = withMatcher((email: string , password: string, displayName: string): SignUpStart =>
-    createAction(USER_ACTION_TYPES.SIGN_UP_START,{ email, password, displayName }));
+    createAction(USER_ACTION_TYPES.SIGN_UP_START, { email, password, displayName }));
 
 export const signUpSuccess = withMatcher((user: User, additionalDetails: AdditionalInformation): SignUpSuccess =>
     createAction(USER_ACTION_TYPES.SIGN_UP_SUCCESS, {user, additionalDetails}));
