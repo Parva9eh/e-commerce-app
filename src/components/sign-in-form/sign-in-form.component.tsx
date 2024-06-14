f9a09cfbc48bd1eebdc3732d6eeb5dd676f3c5af
Parvaneh <22968390+Parva9eh@users.noreply.github.com>
2024-06-13 23:37:35 -0700

    Converted sign in form component to ts

--- src/components/sign-in-form/sign-in-form.component.tsx
+++ src/components/sign-in-form/sign-in-form.component.tsx
@@ -1,4 +1,5 @@
-import {useState} from "react";
+import {useState, FormEvent, ChangeEvent} from "react";
+import { AuthError, AuthErrorCodes } from "@firebase/auth";
 import {SignInContainer, ButtonsContainer} from "./sign-in-form.styles";
 import Button, {BUTTON_TYPE_CLASSES} from "../button/button.component";
 import FormInput from "../form-input/form-input.component";
@@ -23,23 +24,23 @@ const SignInForm = () => {
         setFormFields(defaultFormFields);
       };
 
-    const handleChange = (event) => {
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
         const {name, value} = event.target;
         setFormFields({...formFields, [name]: value})
     };
 
-    const handleSubmit = async (event) => {
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
 
         try {
             dispatch(emailSignInStart(email, password));
             resetFormFields();
         } catch (error) {
-            switch (error.code) {
-            case 'auth/wrong-password':
+            switch ((error as AuthError).code) {
+            case AuthErrorCodes.INVALID_PASSWORD:
                 alert('incorrect password for email');
                 break;
-            case 'auth/user-not-found':
+            case AuthErrorCodes.NULL_USER:
                 alert('no user associated with this email');
                 break;
             default:
