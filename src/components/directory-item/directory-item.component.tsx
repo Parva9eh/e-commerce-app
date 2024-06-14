f2d81e1a9ca4b9b198ef49e592714097c1ea3413
Parvaneh <22968390+Parva9eh@users.noreply.github.com>
2024-06-14 13:59:13 -0700

    Converted directory and directory item to ts

--- src/components/directory-item/directory-item.component.tsx
+++ src/components/directory-item/directory-item.component.tsx
@@ -1,13 +1,19 @@
+import { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {DirectoryItemContainer, BackgroundImage, Body} from './directory-item.styles';
+import { DirectoryCategory } from '../directory/directory.component';
 
-const DirectoryItem = ({ category }) => {
+type DirectoryItemProps = {
+  category: DirectoryCategory;
+};
+
+const DirectoryItem: FC<DirectoryItemProps> = ({ category }) => {
   const navigate = useNavigate();
   const { imageUrl, title, route } = category;
   const onNavigateHandler = () => navigate(route);
   return (
     <DirectoryItemContainer onClick={onNavigateHandler}>
-      <BackgroundImage $imageUrl= {imageUrl} />
+      <BackgroundImage imageUrl= {imageUrl} />
       <Body>
         <h2>{title}</h2>
         <p>Shop Now</p>
