4e6c10049977b5571cb4e26fcc9b1eeeee74eeaf
Parvaneh <22968390+Parva9eh@users.noreply.github.com>
2024-06-13 23:39:34 -0700

    Converted sign up form component to ts

--- src/components/sign-up-form/sign-up-form.component.tsx
+++ src/components/sign-up-form/sign-up-form.component.tsx
@@ -1,4 +1,5 @@
-import {useState} from "react";
+import {useState, FormEvent, ChangeEvent} from "react";
+import { AuthError, AuthErrorCodes } from "@firebase/auth";
 import { useDispatch } from "react-redux";
 import {SignUpContainer} from "./sign-up-form.styles";
 import Button from "../button/button.component";
@@ -22,12 +23,12 @@ const SignUpForm = () => {
         setFormFields(defaultFormFields);
       };
 
-    const handleChange = (event) => {
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
         const {name, value} = event.target;
         setFormFields({...formFields, [name]: value})
     }
 
-    const handleSubmit = async (event) => {
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
 
         if(password !== confirmPassword) {
@@ -39,7 +40,7 @@ const SignUpForm = () => {
             resetFormFields();
 
         }catch(error){
-            if (error.code === 'auth/email-already-in-use') {
+            if ((error as AuthError).code === AuthErrorCodes.EMAIL_EXISTS) {
                 alert('Cannot create user, email already in use');
               } else {
                 console.log('user creation encountered an error', error);
