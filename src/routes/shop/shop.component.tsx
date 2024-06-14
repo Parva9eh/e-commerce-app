4fc8197b9a41c2d742ed70e650897da0933745f6
Parvaneh <22968390+Parva9eh@users.noreply.github.com>
2024-06-14 14:40:10 -0700

    Converted home and shop to ts

--- src/routes/shop/shop.component.tsx
+++ src/routes/shop/shop.component.tsx
@@ -7,9 +7,11 @@ import { fetchCategoriesStart } from '../../store/categories/category.action';
 
 const Shop = () => {
     const dispatch = useDispatch();
+
     useEffect(()=>{
             dispatch(fetchCategoriesStart());
-    },[])
+    },[]);
+    
     return(
         <Routes>
             <Route index element={<CategoriesPreview />} />
