4a4723c2aba11d68eeef5ce32128d785a9866e43
Parvaneh <22968390+Parva9eh@users.noreply.github.com>
2024-06-16 20:08:25 -0700

    Converted product card to ts

--- src/components/product-card/product-card.component.tsx
+++ src/components/product-card/product-card.component.tsx
@@ -1,17 +1,23 @@
+import { FC } from "react";
 import {ProductCardContainer, Footer, Name, Price} from "./product-card.styles";
 import { useDispatch, useSelector } from "react-redux";
 import Button, {BUTTON_TYPE_CLASSES} from "../button/button.component";
 import { selectCartItems } from "../../store/cart/cart.selector";
 import { addItemToCart } from "../../store/cart/cart.action";
+import { CategoryItem } from '../../store/categories/category.types';
 
-const ProductCard = ({product}) =>{
+type ProductCardProps = {
+    product: CategoryItem;
+  };
+
+const ProductCard: FC<ProductCardProps> = ({product}) =>{
     const dispatch = useDispatch();
     const {name, price, imageUrl} = product;
     const cartItems = useSelector(selectCartItems);
     const addProductToCart = () => dispatch(addItemToCart(cartItems, product));
     return(
         <ProductCardContainer>
-            <img src={imageUrl} alt={`${name}`}/>
+            <img src={`${imageUrl}`} alt={`${name}`}/>
                 <Footer>
                     <Name>{name}</Name>
                     <Price>{price}</Price>
