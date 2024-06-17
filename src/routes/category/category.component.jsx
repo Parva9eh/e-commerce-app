fa0e1075fc139896442e81883657a446ace2bcab
Parvaneh <22968390+Parva9eh@users.noreply.github.com>
2024-06-16 17:41:02 -0700

    Modified category to use gql query instead of context

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,18 +1,40 @@
-import {useContext, useEffect, useState, Fragment} from "react";
-import {useParams} from 'react-router-dom';
-import { CategoriesContext } from "../../context/categories.context";
+import { useEffect, useState, Fragment } from "react";
+import { useParams } from 'react-router-dom';
 import ProductCard from "../../components/product-card/product-card.component";
 import {CategoryContainer, Title} from './category.styles';
 import Spinner from "../../components/spinner/spinner.components";
+import {gql, useQuery} from '@apollo/client';
+
+const GET_CATEGORY = gql`
+    query($title: String!){
+        getCollectionsByTitle(title: $title){
+            id
+            title
+            items{
+                id
+                name
+                price
+                imageUrl
+            }
+        }
+    }
+`
 
 const Category = () =>{
     const {category} = useParams();
-    const {categoriesMap, loading} = useContext(CategoriesContext);
-    const [products, setProducts] = useState(categoriesMap[category]);
-
+    const [products, setProducts] = useState([]);
+    const {data, error, loading } = useQuery(GET_CATEGORY, {
+        variables: {
+            title: category
+        }
+    });
     useEffect(()=>{
-        setProducts(categoriesMap[category]);
-    },[category, categoriesMap])
+        if(data){
+            const {getCollectionsByTitle: {items},} = data;
+            setProducts(items);
+        }
+        
+    },[category, data]);
     
     return(
         <Fragment>
@@ -27,7 +49,6 @@ const Category = () =>{
                     </Fragment>
                 )
             }
-            
         </Fragment>
     )
 }
