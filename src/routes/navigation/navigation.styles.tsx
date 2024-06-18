3a19dc99abe3bbda51419f6571cf46a93eb5ac7d
Parvaneh <22968390+Parva9eh@users.noreply.github.com>
2024-06-17 19:32:12 -0700

    Updated styles to use media queries

--- src/routes/navigation/navigation.styles.tsx
+++ src/routes/navigation/navigation.styles.tsx
@@ -7,16 +7,27 @@ export const NavigationContainer = styled.div`
   display: flex;
   justify-content: space-between;
   margin-bottom: 25px;
+
+  @media screen and (max-width: 800px) {
+    height: 60px;
+    padding: 10px 20px;
+    margin-bottom: 20px;
+  }
 `;
 
 export const LogoContainer = styled(Link)`
   height: 100%;
   width: 70px;
   padding: 25px;
+
+  @media screen and (max-width: 800px) {
+    width: 50px;
+    padding: 0;
+  }
 `;
 
 export const NavLinks = styled.div`
-width: 50%;
+  width: 50%;
   height: 100%;
   display: flex;
   align-items: center;
@@ -26,5 +37,9 @@ width: 50%;
 export const NavLink = styled(Link)`
   padding: 10px 15px;
   cursor: pointer;
+
+  @media screen and (max-width: 800px) {
+    width: 80%;
+  }
 `;
 
